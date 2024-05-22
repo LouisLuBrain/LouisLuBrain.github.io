800ab95d69b322a33faf2de4a49cef74b5dfeb39
yizhou <1394853556@qq.com>
2024-05-22 23:18:59 +0800

    fix: add layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,20 @@ import { Calendar } from "./components";
 
 function App() {
   return (
-    <div className="w-full h-full min-h-screen bg-slate-50 py-10 px-20">
-      <Calendar />
+    <div className="w-full h-full min-h-screen bg-gray-200 py-10 px-20">
+      <div className="grid grid-cols-3 w-full gap-6">
+        <div className="flex">
+          <Calendar />
+        </div>
+        <div className="flex"></div>
+        <div className="flex"></div>
+        <div className="flex"></div>
+        <div className="flex"></div>
+        <div className="flex"></div>
+        <div className="flex"></div>
+        <div className="flex"></div>
+        <div className="flex"></div>
+      </div>
     </div>
   );
 }
