178ba84f1d49dab5189338c9b90120bbb8c72c90
yizhou <1394853556@qq.com>
2025-02-06 00:08:20 +0800

    feat: enhance DailyPage and IssueList components with loading state and data fetching

--- src/components/issue-list/IssueList.tsx
+++ src/components/issue-list/IssueList.tsx
@@ -1,11 +1,33 @@
-export function IssueList() {
+import { IconLoader2 } from "@tabler/icons-react";
+import { Item } from "../../types";
+
+interface Props {
+  loading?: boolean;
+  issues?: Item[];
+}
+
+export function IssueList({ loading, issues }: Props) {
   return (
-    <div className="rounded-lg bg-slate-50 border-stone-200 border-2 p-4">
+    <div className="rounded-lg w-full bg-slate-50 shadow-sm border-2 p-4 space-y-4">
       <div className="text-xl font-semibold text-black">Issues:</div>
 
-      <div className="w-full flex">
-        <div>item</div>
-      </div>
+      {loading ? (
+        <div className="w-full flex justify-center">
+          <IconLoader2 className="animate-spin text-stone-700" size={32} />
+        </div>
+      ) : (
+        <div className="w-full flex flex-col gap-2">
+          {issues?.map((issue) => (
+            <div
+              key={issue.uuid}
+              className="w-full p-2 hover:bg-stone-200 cursor-pointer rounded-lg overflow-hidden"
+            >
+              <div className="text-lg font-semibold">{issue.title}</div>
+              <div className="text-sm">{issue.content}</div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/DailyPage.tsx
+++ src/pages/DailyPage.tsx
@@ -1,22 +1,59 @@
 import { Moment } from "moment";
 import { Calendar, IssueList } from "../components";
-import { useState } from "react";
+import { useMemo, useState } from "react";
+import { Item } from "../types";
 
 export function DailyPage() {
   const [selectedDay, setSelectedDay] = useState<Moment | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [dayData, setDayData] = useState<Item[]>();
 
   const handleSelect = (date: Moment) => {
     setSelectedDay(date);
   };
 
+  useMemo(() => {
+    if (!selectedDay) return;
+    setLoading(true);
+    getDayData({ date: selectedDay.format("YYYY-MM-DD") }).then((data) => {
+      setDayData(data.issues);
+      setLoading(false);
+    });
+  }, [selectedDay]);
+
   return (
     <div className="flex justify-center bg-slate-200 h-full">
       <div className="flex gap-2 p-4 w-full flex-wrap">
         <Calendar onSelect={handleSelect} />
-        <div className="flex-1">
-          <IssueList />
+        <div className="flex-1 min-w-40 flex">
+          <IssueList loading={loading} issues={dayData} />
         </div>
       </div>
     </div>
   );
+}
+
+async function getDayData(params: { date: string }) {
+  const { date } = params;
+  console.log("Fetching data for date: ", date);
+  const response = await new Promise<{ issues: Item[] }>((resolve) => {
+    setTimeout(() => {
+      resolve({
+        issues: [
+          {
+            uuid: "d6FbEB59-c3ae-FA84-0cac-43b3Fd024E14",
+            title: "Issue 1",
+            content: "This is the first issue",
+          },
+          {
+            uuid: "Ab5aDfA6-baAc-eA9A-C4B5-63f0AF660bc9",
+            title: "Issue 2",
+            content: "This is the second issue",
+          },
+        ],
+      });
+    }, 2000);
+  });
+  const data = response;
+  return data;
 }
