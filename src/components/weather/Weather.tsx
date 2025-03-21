511d6476233282459058c24b97a0757b6c54794d
yizhou <1394853556@qq.com>
2025-03-21 22:56:09 +0800

    feat(weather): refactor Weather component for improved data handling and UI enhancements

--- src/components/weather/Weather.tsx
+++ src/components/weather/Weather.tsx
@@ -4,27 +4,28 @@ import get from "lodash/get";
 import { WeatherData, DailyWeather } from "./types";
 import {
   getWeatherData,
-  weatherIcon,
   getLocationData,
   storeLocation,
   getLocation,
 } from "./weatherAPI";
 import { IconRefresh } from "@tabler/icons-react";
+import { weatherIcon } from "./icons";
 
 export function Weather() {
   const [location, setLocation] = useState(getLocation().name);
   const [currentWeather, setCurrentWeather] = useState<WeatherData>(
     {} as WeatherData
   );
-  const [dailyWeather, setDailyWeather] = useState<DailyWeather[]>([]);
+  const [dailyWeather] = useState<DailyWeather[]>([]);
   const [loading, setLoading] = useState(true);
 
   const fetchWeatherData = async (lat: number, lon: number) => {
     try {
       setLoading(true);
-      const res = await getWeatherData(lat, lon, "minutely,hourly");
-      setDailyWeather(get(res, "daily", []));
-      setCurrentWeather(res);
+      const res = await getWeatherData(lat, lon);
+      // setDailyWeather(get(res, "data", []));
+      setCurrentWeather(res.data[0]);
+      storeLocation({ name: res.data[0].city_name, lat, lon });
     } catch (err) {
       console.error(err);
       // Add error notification
@@ -58,7 +59,6 @@ export function Weather() {
         setLoading(true);
         const position = await getCurrentPosition();
         const { latitude: lat, longitude: lon } = position.coords;
-        storeLocation({ name: "Current Location", lat, lon });
         await fetchWeatherData(lat, lon);
       } catch (error) {
         console.error("Failed to get location:", error);
@@ -108,18 +108,17 @@ export function Weather() {
     get(item, "weather[0].icon", "01d") as keyof typeof weatherIcon;
 
   return (
-    <div className="rounded-lg w-full bg-slate-50 shadow-md border-2 p-4 space-y-4 flex flex-col">
+    <div className="rounded-lg w-full max-w-96 self-end bg-slate-900 shadow-lg border-2 p-4 space-y-4 flex flex-col">
       <div className="flex flex-col">
         <div className="flex justify-between items-baseline">
           <input
-            className="font-bold text-xl ml-3 cursor-text hover:bg-gray-100 focus:bg-gray-100 px-1 rounded"
-            style={{ color: "#309FFB" }}
+            className="font-bold text-xl ml-3 cursor-text bg-transparent text-blue-400 px-1 rounded outline-0"
             value={location}
             onChange={(e) => setLocation(e.target.value)}
             onBlur={handleLocationChange}
           />
           <IconRefresh
-            className={`cursor-pointer text-gray-600 hover:text-blue-main text-lg ${
+            className={`cursor-pointer text-gray-200 hover:text-blue-main text-lg ${
               loading ? "animate-spin" : ""
             }`}
             onClick={handleRefresh}
@@ -134,22 +133,22 @@ export function Weather() {
                 weatherIcon[
                   get(
                     currentWeather,
-                    "weather[0].icon",
+                    "weather.icon",
                     "01d"
                   ) as keyof typeof weatherIcon
                 ]
               }
               alt="weather"
             />
-            <div className="text-sm text-gray-500 px-2">
-              {get(currentWeather, "weather[0].description", "Unknown")}
+            <div className="text-sm text-gray-200 px-2">
+              {get(currentWeather, "weather.description", "Unknown")}
             </div>
           </div>
 
-          <div>
+          <div className="cursor-default">
             <div className="flex items-start h-16">
               <span className="font-bold text-5xl" style={{ color: "#309FFB" }}>
-                {temp(currentWeather?.main?.temp)}
+                {temp(currentWeather?.temp)}
               </span>
               <img
                 className="w-6 h-6 inline-block"
@@ -157,8 +156,8 @@ export function Weather() {
                 alt="celcius"
               />
             </div>
-            <div className="text-sm text-gray-500 pl-1">
-              Feels like: {temp(currentWeather?.main?.feels_like)}
+            <div className="text-sm text-gray-200 pl-1">
+              Feels like: {temp(currentWeather?.app_temp)}
               <img
                 className="w-2 h-2 inline-block align-top"
                 src={weatherIcon.Celcius}
@@ -168,41 +167,45 @@ export function Weather() {
           </div>
         </div>
 
-        {!loading ? (
-          <div className="flex justify-between pt-4 pb-2 sm:text-xs text-base text-gray-500">
-            <WeatherInfo
-              icon={weatherIcon.Humidity}
-              value={`${currentWeather?.main?.humidity ?? 0}%`}
-              label="Humidity"
-            />
-            {/* <WeatherInfo
-              icon={weatherIcon.uvi}
-              value={currentWeather?.main?.uvi ?? "0"}
-              label="UV Index"
-            /> */}
-            <WeatherInfo
-              icon={weatherIcon.SunRise}
-              value={
-                currentWeather?.sys?.sunrise
-                  ? moment.unix(currentWeather?.sys?.sunrise).format("HH:MM")
-                  : "??:??"
-              }
-              label="Sunrise"
-            />
-            <WeatherInfo
-              icon={weatherIcon.Sunset}
-              value={
-                currentWeather?.sys?.sunset
-                  ? moment.unix(currentWeather?.sys?.sunset).format("HH:MM")
-                  : "??:??"
-              }
-              label="Sunset"
-            />
-          </div>
-        ) : null}
+        <div className="flex justify-between pt-4 pb-2 sm:text-xs text-base text-gray-200">
+          <WeatherInfo
+            icon={weatherIcon.Humidity}
+            value={`${currentWeather?.rh ?? 0}%`}
+            label="Humidity"
+          />
+          <WeatherInfo
+            icon={weatherIcon.uvi}
+            value={currentWeather?.uv ?? "0"}
+            label="UV Index"
+          />
+          <WeatherInfo
+            icon={weatherIcon.SunRise}
+            value={
+              currentWeather?.sunrise
+                ? moment
+                    .utc(currentWeather?.sunrise, "HH:mm")
+                    .local()
+                    .format("HH:mm")
+                : "??:??"
+            }
+            label="Sunrise"
+          />
+          <WeatherInfo
+            icon={weatherIcon.Sunset}
+            value={
+              currentWeather?.sunset
+                ? moment
+                    .utc(currentWeather?.sunset, "HH:mm")
+                    .local()
+                    .format("HH:mm")
+                : "??:??"
+            }
+            label="Sunset"
+          />
+        </div>
 
         {!loading ? (
-          <div className="flex justify-between sm:text-xs text-base text-gray-500 flex-wrap">
+          <div className="flex justify-between sm:text-xs text-base text-gray-200 flex-wrap">
             {dailyWeather.map((item) => (
               <DailyWeatherCard
                 key={item.dt}
@@ -217,8 +220,6 @@ export function Weather() {
             ))}
           </div>
         ) : null}
-
-        {loading && <div className="mx-auto">Loading...</div>}
       </div>
     </div>
   );
@@ -234,14 +235,14 @@ function WeatherInfo({
   label: string;
 }) {
   return (
-    <div className="flex items-center sm:flex-row flex-col transition-all duration-200 hover:bg-gray-100 hover:shadow-md rounded-md p-1 flex-1 justify-center">
+    <div className="flex items-center sm:flex-row flex-col transition-all duration-300 hover:bg-slate-700 cursor-pointer hover:shadow-lg rounded-md p-1 flex-1 justify-center">
       <img
         className="sm:w-6 w-8 sm:h-6 h-8 inline-block"
         src={icon}
         alt={label}
       />
       <span className="px-1 align-middle whitespace-nowrap">
-        <span className="sm:hidden text-xs text-gray-600 mr-1">{label}</span>
+        <span className="sm:hidden text-xs text-gray-200 mr-1">{label}</span>
         {value}
       </span>
     </div>
